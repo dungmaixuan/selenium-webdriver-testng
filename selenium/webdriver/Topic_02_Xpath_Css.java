d8285dea8f8c7e3f071071b6e4ac6079cc5d213f
Mai Xuan Dung <dungmaixuan.1808@gmail.com>
2022-01-14 22:54:04 +0700

    update Topic_02 & 03

--- selenium/webdriver/Topic_02_Xpath_Css.java
+++ selenium/webdriver/Topic_02_Xpath_Css.java
@@ -1,5 +1,15 @@
 package webdriver;
 
 public class Topic_02_Xpath_Css {
+	
+	public void TC_01() {
+		
+		
+	}
+	
+    public void TC_02() {
+		
+		
+	}
 
 }
--- selenium/webdriver/Topic_03_Textbox_TextArea.java
+++ selenium/webdriver/Topic_03_Textbox_TextArea.java
@@ -1,5 +1,10 @@
 package webdriver;
 
 public class Topic_03_Textbox_TextArea {
+	
+	public void TC_01() {
+		
+		
+	}
 
 }
